04bafa1e229f971f4131b55bde69be7394137f9f
Kevin <kevinguan888@hotmail.com>
2020-04-10 10:05:34 +1200

    added placeholder facts

--- client/components/Timer.jsx
+++ client/components/Timer.jsx
@@ -10,20 +10,46 @@ class Timer extends React.Component {
     }
 
     render() {
-            return (
+        return (
             <>
-            <h2>Ready? {this.state.count} </h2>
+              
+                <div>
+                    <h2>Facts</h2>
+                    <ul>
+                        <li>Super Kawaii</li>
+                        <li>Basketball God</li>
+                        <li>Hype Beast</li>
+                        <li>Korean Oppa</li>
+                        <li>Ramen is staple food for this god</li>
+                    </ul>
+                </div>
+
+                <button>Start game</button>
+                <h2>Ready? {this.state.count} </h2>
+
+                <div>
+                    <h2>Facts pt 2</h2>
+                    <ul>
+                        <li>IQ over 9000</li>
+                        <li>Has a Nintendo Switch and paid off his debt in Animal Crossing, husband material much??</li>
+                        <li>Smile brighter than our futures</li>
+                        <li>Did i mention korean oppa?</li>
+                        <li>Read all other facts again</li>
+                    </ul>
+                </div>
+
             </>
         )
     }
 
-  componentDidMount() {
-    setInterval(() => {
-      this.setState({
-        count: this.state.count -1 
-      })
-    }, 1000)
-  }
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({
+                count: this.state.count - 1
+            })
+        }, 1000)
+    }
+
 }
 
 export default Timer
