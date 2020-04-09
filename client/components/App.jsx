567a99f0722f871770e55f3ddf184a702c5dcfc9
Richard Kwon <rkminjae@gmail.com>
2020-04-10 09:31:37 +1200

    Setup react router

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <Router>
+      {/* Routes go here */}
+      <Route />
+    </Router>
   )
 }
 
