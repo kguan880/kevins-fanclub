e6e1b6499b70dd4296d848429f8a8f23274c56fa
Kevin <kevinguan888@hotmail.com>
2020-04-10 10:35:41 +1200

    set conditional for facts

--- client/components/Timer.jsx
+++ client/components/Timer.jsx
@@ -10,46 +10,57 @@ class Timer extends React.Component {
     }
 
     render() {
+
         return (
             <>
-              
-                <div>
-                    <h2>Facts</h2>
-                    <ul>
-                        <li>Super Kawaii</li>
-                        <li>Basketball God</li>
-                        <li>Hype Beast</li>
-                        <li>Korean Oppa</li>
-                        <li>Ramen is staple food for this god</li>
-                    </ul>
-                </div>
+                {this.state.count > 0 &&
+                    <div>
+                        <h2>Facts</h2>
+                        <ul>
+                            <li>Super Kawaii</li>
+                            <li>Basketball God</li>
+                            <li>Hype Beast</li>
+                            <li>Korean Oppa</li>
+                            <li>Ramen is staple food for this god</li>
+                        </ul>
+                    </div>
+                }
+
 
                 <button>Start game</button>
-                <h2>Ready? {this.state.count} </h2>
 
-                <div>
-                    <h2>Facts pt 2</h2>
-                    <ul>
-                        <li>IQ over 9000</li>
-                        <li>Has a Nintendo Switch and paid off his debt in Animal Crossing, husband material much??</li>
-                        <li>Smile brighter than our futures</li>
-                        <li>Did i mention korean oppa?</li>
-                        <li>Read all other facts again</li>
-                    </ul>
-                </div>
+                {this.state.count > 0 &&
+                    <h2>Timer: {this.state.count} </h2>
+                }
 
+                {this.state.count > 0 &&
+                    <div>
+                        <h2>Facts pt 2</h2>
+                        <ul>
+                            <li>IQ over 9000</li>
+                            <li>Has a Nintendo Switch and paid off his debt in Animal Crossing, greatest husbando??</li>
+                            <li>Smile brighter than our futures</li>
+                            <li>Did i mention korean oppa?</li>
+                            <li>Read all other facts again</li>
+                        </ul>
+                    </div>
+                }
             </>
         )
     }
 
     componentDidMount() {
-        setInterval(() => {
+       const interval = setInterval(() => {
             this.setState({
                 count: this.state.count - 1
             })
         }, 1000)
     }
 
+    componentWillUnmount(){
+        clearInterval(this.interval)
+    }
+    
 }
 
 export default Timer
